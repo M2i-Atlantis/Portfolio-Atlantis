a5be47c3074e3e9c17b975019506b6c60d4c861b
Aurore PHILIPPE <philip.aurore@hotmail.fr>
2021-10-01 14:25:04 +0200

    correction bug setId

--- src/models/BaseEntity.php
+++ src/models/BaseEntity.php
@@ -2,11 +2,6 @@
 
 namespace App\models;
 
-// Défini ce qu'est une entité
-// Une entité possède un id.
-// Toutes les entités doivent hérités de cette classe.
-// TODO: Est ce qu'on ajoute la date de modification / création ?
-
 abstract class BaseEntity {
     private int $id;
 
@@ -17,7 +12,7 @@ abstract class BaseEntity {
 
     public function setId(int $id): self
     {
-        return $this->id = $id;
+        $this->id = $id;
         return $this;
     }
 }
