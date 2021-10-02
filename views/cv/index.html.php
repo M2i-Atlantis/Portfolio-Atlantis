8c7222c4564d50c63a9a8f66aa75a387bac83aa7
julien-gabard <juliengabard@hotmail.fr>
2021-10-02 21:03:10 +0200

    add remark in html cv

--- views/cv/index.html.php
+++ views/cv/index.html.php
@@ -1,12 +1,28 @@
 <div class="container my-4">
-    <h1 class="text-center mb-3">CV</h1>
+    <h1 class="text-center mb-3">
+        Nom d'utilisateur
+    </h1>
 
+    <!-- About -->
     <div class="card my-4">
-        <div class="card-header">
+        <div class="card-header bg-info">
+            <h2 class="card-title text-center">A propos</h2>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Harum fugit officia nam repellendus voluptates quae dolore placeat aspernatur, 
+            itaque molestias voluptatibus recusandae culpa quia, 
+            dignissimos repellat ullam deleniti, sequi vitae!
+        </div>
+    </div>
+
+    <!-- Experiences -->
+    <div class="card my-4">
+        <div class="card-header bg-info">
             <h2 class="card-title text-center">Expériences</h2>
         </div>
         <div class="card-body">
-            <div class="card text-dark bg-info mb-3">
+            <div class="card mb-3">
                 <div class="card-header">
                     <h5 class="card-title">
                         Auchan
@@ -20,12 +36,13 @@
         </div>
     </div>
 
+    <!-- Projects -->
     <div class="card my-4">
-        <div class="card-header">
+        <div class="card-header bg-info">
             <h2 class="card-title text-center">Projets</h2>
         </div>
         <div class="card-body">
-            <div class="card text-dark bg-info mb-3">
+            <div class="card mb-3">
                 <div class="card-header">
                     <h5 class="card-title">Portfolio</h5>
                     <span>Du 10/05/2019 au 20/01/2021</span>
@@ -37,24 +54,16 @@
                     <span class="badge bg-dark">Windows</span>
                 </div>
             </div>
-            <div class="card text-dark bg-info mb-3">
-                <div class="card-header">
-                    <h5 class="card-title">Oshop</h5>
-                    <span>Du 10/05/2019 au 20/01/2021</span>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                </div>
-            </div>
         </div>
     </div>
 
+    <!-- Trainings -->
     <div class="card my-4">
-        <div class="card-header">
+        <div class="card-header bg-info">
             <h2 class="card-title text-center">Formations</h2>
         </div>
         <div class="card-body">
-            <div class="card text-dark bg-info mb-3">
+            <div class="card mb-3">
                 <div class="card-header">
                     <h5 class="card-title">
                         O'clock
