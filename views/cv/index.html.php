b7f53f84fb1604a6fcb69ced2de715368ce94ba5
julien-gabard <juliengabard@hotmail.fr>
2021-10-02 18:14:10 +0200

    editing html cv

--- views/cv/index.html.php
+++ views/cv/index.html.php
@@ -9,7 +9,6 @@
             <div class="card text-dark bg-info mb-3">
                 <div class="card-header">
                     <h5 class="card-title">
-                        <span class="badge bg-dark">CDD</span>
                         Auchan
                     </h5>
                     <span>Du 10/05/2019 au 20/01/2021</span>
@@ -33,14 +32,33 @@
                 </div>
                 <div class="card-body">
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <span class="badge rounded-pill bg-dark">PHP</span>
-                    <span class="badge rounded-pill bg-dark">MVC</span>
-                    <span class="badge rounded-pill bg-dark">Windows</span>
+                    <span class="badge bg-dark">PHP</span>
+                    <span class="badge bg-dark">MVC</span>
+                    <span class="badge bg-dark">Windows</span>
                 </div>
             </div>
             <div class="card text-dark bg-info mb-3">
                 <div class="card-header">
                     <h5 class="card-title">Oshop</h5>
+                    <span>Du 10/05/2019 au 20/01/2021</span>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card my-4">
+        <div class="card-header">
+            <h2 class="card-title text-center">Formations</h2>
+        </div>
+        <div class="card-body">
+            <div class="card text-dark bg-info mb-3">
+                <div class="card-header">
+                    <h5 class="card-title">
+                        O'clock
+                    </h5>
                     <span>Du 10/05/2019 au 20/01/2021</span>
                 </div>
                 <div class="card-body">
