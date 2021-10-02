18f16d5770994f4e4309a8bd9c9c22853c44273f
julien-gabard <juliengabard@hotmail.fr>
2021-10-02 18:08:29 +0200

    Editing exemple CV

--- views/cv/index.html.php
+++ views/cv/index.html.php
@@ -1,5 +1,52 @@
 <div class="container my-4">
-    <h2 class="text-center">CV</h2>
+    <h1 class="text-center mb-3">CV</h1>
 
-    <p>Futur contenu...</p>
+    <div class="card my-4">
+        <div class="card-header">
+            <h2 class="card-title text-center">Expériences</h2>
+        </div>
+        <div class="card-body">
+            <div class="card text-dark bg-info mb-3">
+                <div class="card-header">
+                    <h5 class="card-title">
+                        <span class="badge bg-dark">CDD</span>
+                        Auchan
+                    </h5>
+                    <span>Du 10/05/2019 au 20/01/2021</span>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card my-4">
+        <div class="card-header">
+            <h2 class="card-title text-center">Projets</h2>
+        </div>
+        <div class="card-body">
+            <div class="card text-dark bg-info mb-3">
+                <div class="card-header">
+                    <h5 class="card-title">Portfolio</h5>
+                    <span>Du 10/05/2019 au 20/01/2021</span>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <span class="badge rounded-pill bg-dark">PHP</span>
+                    <span class="badge rounded-pill bg-dark">MVC</span>
+                    <span class="badge rounded-pill bg-dark">Windows</span>
+                </div>
+            </div>
+            <div class="card text-dark bg-info mb-3">
+                <div class="card-header">
+                    <h5 class="card-title">Oshop</h5>
+                    <span>Du 10/05/2019 au 20/01/2021</span>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
