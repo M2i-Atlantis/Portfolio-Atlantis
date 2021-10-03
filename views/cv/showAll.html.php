e49a309b59ffc3d94e72d62e75da703e2fa5bb49
julien-gabard <juliengabard@hotmail.fr>
2021-10-03 16:44:28 +0200

    Edit html all cv

--- views/cv/showAll.html.php
+++ views/cv/showAll.html.php
@@ -1,5 +1,24 @@
 <div class="container my-4">
-    <h1 class="text-center">CV</h1>
+    <h1 class="text-center my-4">CV</h1>
 
-    
+    <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Gabard Julien</h5>
+                <small>Mise à jour il y a 3 jours</small>
+            </div>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Toto Larue</h5>
+                <small>Mise à jour il y a 1 heure</small>
+            </div>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Marie Lafrange</h5>
+                <small>Mise à jour il y a 3 semaines</small>
+            </div>
+        </a>
+    </div>
 </div>
