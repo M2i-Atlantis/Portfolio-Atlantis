f8591a13dfc7aa6821e47749dc64188f87e1b765
julien-gabard <juliengabard@hotmail.fr>
2021-10-03 10:01:16 +0200

    add search in navbar

--- views/layout.html.php
+++ views/layout.html.php
@@ -48,6 +48,11 @@
                         <?php endif; ?>
                     </ul>
                 </div>
+
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-primary" type="submit">Search</button>
+                </form>
             </div>
         </nav>
     </header>
